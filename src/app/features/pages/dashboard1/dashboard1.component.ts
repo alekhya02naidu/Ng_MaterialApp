ad8de8dfc747dcf6004c00ede9196377a8533cf4
alekhya02naidu <alekhyalsvnaidu@gmail.com>
2024-08-28 19:49:58 +0530

    updated navbar for sm

--- src/app/features/pages/dashboard1/dashboard1.component.ts
+++ src/app/features/pages/dashboard1/dashboard1.component.ts
@@ -10,5 +10,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './dashboard1.component.scss'
 })
 export class Dashboard1Component {
+  revenueCards = [
+    { heading: 'Investments', badgeTitle: 'Monthly', amount: '$47,000', progress: 55 },
+    { heading: 'Sales', badgeTitle: 'Annual', amount: '$22,784', progress: 42 },
+    { heading: 'Cost', badgeTitle: 'Today', amount: '$1,587', progress: 75 },
+    { heading: 'Profit', badgeTitle: 'Weekly', amount: '$1,400', progress: 48 }
+  ];
 
+  getCardClasses(index: number): string {
+    return index % 2 === 0 
+      ? 'bg-orange-100 text-orange-600 progress-bar-orange' 
+      : 'bg-gray-100 text-gray-600 progress-bar-gray';
+  }
 }
